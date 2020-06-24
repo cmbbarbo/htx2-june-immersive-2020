d3fd6ad1cf30c5edec94e098789d2a723f7948ce
Cainan Barboza <CainanBarboza1@gmail.com>
2020-06-24 10:30:00 -0500

    g

---
+++ week04/2-Tuesday/js/script.js
@@ -0,0 +1,2 @@
+console.log("Im located in an external javascript file")
+document.body.innerHTML = "Cainan" 
--- week04/2-Tuesday/labs/lab.js
+++ week04/2-Tuesday/labs/lab.js
@@ -2,24 +2,41 @@
 // Declare a variable called "firstName" whose value is your first name 
 // Declare a variable called "lastName" whose value is your first name 
 // Declare a variable called "name" whose value is the concatenation of firstName and lastName
+// var firstName = "Cainan";
+// var lastName = "Barboza";
+// var name = firstName + lastName;
 
-// Print name to the console
+// console.log(name);
+// // Print name to the console
 
+// var a = 2;
+// // Declare a variable called a with a value of 2 
+// var b = 3;
+// //Declare a variable called b witha value of 3 
+// var x = a + b;
+// // Declare a variable x whose value is a + b 
+// console.log(x);
+// // Print x  to the screen 
 
-// Declare a variable called a with a value of 2 
-
-//Declare a variable called b witha value of 3 
-
-// Declare a variable x whose value is a + b 
-
-// Print x  to the screen 
-
-// Add the string "5" to x and print the result
-
-// Print the number of characters in firstName
-
+// // Add the string "5" to x and print the result
+// x += "5";
+// console.log(x);
+// // Print the number of characters in firstName
+// console.log(firstName.length);
 
 // If else, fizz buzz
+
+for(let i = 1; i <=100; i++){
+    if(i % 15 == 0){
+        console.log("FizBuzz");
+    }else if(i % 3 == 0){
+        console.log("Fizz");
+    }else if(i % 5 == 0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
+}
 
 // ternary operator
 
